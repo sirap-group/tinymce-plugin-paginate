ddd81ff2f2f71308bc8a330a38a4165d36bc7348
Rémi Becheras <rbecheras@sirap.fr>
2016-02-18 16:33:34 +0100

    refactor main.js event handlers

--- src/main.js
+++ src/main.js
@@ -17,45 +17,92 @@ var ui = require('./utils/ui');
 
 tinymce.PluginManager.add('paginate', function(editor) {
 
-  function initPaginator(){
-    if (!paginator) {
-      paginator = new Paginator('A4','portrait', editor);
-      // Create and display pages navigation buttons
-      ui.appendNavigationButtons(paginator);
-    }
-    paginator.init();
-  }
+  /**
+   * Debug all useful editor events to see the order of their happen
+   * @function
+   * @private
+   */
+  function _debugEditorEvents(){
+    var myevents = [];
+    var mycount = {
+      init: 0,
+      change: 0,
+      nodechange: 0,
+      setcontent: 0
+    };
 
-  var display;
-  var paginator;
-
-
-  editor.once('change',function(evt){
-    // Instantiate the paginator
-    initPaginator();
-  });
-  editor.on('init',function(evt){
-    // Instantiate the paginator
-    initPaginator();
-
+    editor.on('init',function(evt){
+      console.log(editor);
+      myevents.push({'init':evt});
+      mycount.init ++;
+      console.log(myevents,mycount);
+    });
+    editor.on('change',function(evt){
+      myevents.push({'change':evt});
+      mycount.change ++;
+      console.log(myevents,mycount);
+    });
+    editor.on('NodeChange',function(evt){
+      myevents.push({'NodeChange':evt});
+      mycount.nodechange ++;
+      console.log(myevents,mycount);
+    });
     editor.on('SetContent',function(evt){
-      initPaginator();
+      myevents.push({'SetContent':evt});
+      mycount.setcontent ++;
+      console.log(myevents,mycount);
     });
 
-    editor.on('NodeChange',function(evt){
+    window.logEvents = myevents;
+    window.logCount = mycount;
+  }
+
+  /**
+  * A 'Paginator' object to handle all paginating behaviors.
+  * @var {Paginator} paginator
+  * @global
+  */
+  var paginator;
+
+  /**
+   * A 'Display' object to handle graphics behaviors for the paginator needs.
+   * @var {Display} display
+   * @private
+   */
+  var display;
+
+  /**
+   * Is set to true when paginator is initialized.
+   * @var {Boolean} paginatorStartListening
+   * @private
+   */
+  var paginatorStartListening = false;
+
+  editor.once('init',function(){
+    paginator = new Paginator('A4','portrait', editor);
+    !paginatorStartListening && paginator.init();
+    paginatorStartListening = true;
+    ui.appendNavigationButtons(paginator);
+  });
+  editor.once('change',function(){
+    paginatorStartListening = !!paginator;
+    paginatorStartListening && paginator.init();
+  });
+  editor.on('change',function(){
+    paginatorStartListening && paginator.watchPage();
+  });
+  editor.on('SetContent',function(){
+    // paginatorStartListening && paginator.init();
+  });
+  editor.on('NodeChange',function(){
+    if (paginatorStartListening) {
       try {
         paginator.gotoFocusedPage();
       } catch (e) {
-        console.info('cant go to focused page now');
-        console.error(e);
+        console.info('Can\'t go to focused page now.');
         console.error(e.stack);
       }
-    });
-
-    editor.on('change',function(evt){
-      paginator.watchPage();
-    });
-
+    }
   });
 
 });
