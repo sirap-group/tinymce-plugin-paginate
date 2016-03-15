535836c0c2cbc15792e1cd6bba282d769089a601
Rémi Becheras <rbecheras@sirap.fr>
2016-03-15 15:53:56 +0100

    push to gh-sirap-group when grunt bump

--- gruntfile.js
+++ gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
           tagName: 'v%VERSION%',
           tagMessage: 'Version %VERSION%',
           push: true,
-          pushTo: 'origin',
+          pushTo: 'gh-sirap-group',
           gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
           globalReplace: false,
           prereleaseName: false,
