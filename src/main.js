970b5148ddbc1625c3885a51a123c7a98950868b
Rémi Becheras <rbecheras@sirap.fr>
2016-05-04 16:54:50 +0200

    refactor main.js against standard coding style

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * plugin.js
@@ -28,41 +28,45 @@
  * @version 1.0.0
  */
 
-
-/**
- * Tinymce library - injected by the plugin loader.
- * @external tinymce
- * @see {@link https://www.tinymce.com/docs/api/class/tinymce/|Tinymce API Reference}
- */
-/*global tinymce:true */
-
 /**
  * The jQuery plugin namespace - plugin dependency.
  * @external "jQuery.fn"
  * @see {@link http://learn.jquery.com/plugins/|jQuery Plugins}
  */
-/*global jquery:true */
+
+var $ = window.jquery
+
+/**
+ * Tinymce library - injected by the plugin loader.
+ * @type {object}
+ * @external tinymce
+ * @see {@link https://www.tinymce.com/docs/api/class/tinymce/|Tinymce API Reference}
+ */
+var tinymce = window.tinymce
 
 /**
  * Paginator class
  * @type {Paginator}
  * @global
  */
-var Paginator = require('./classes/Paginator');
+var Paginator = require('./classes/Paginator')
 
 /**
  * Paginator ui module
  * @type {object}
  * @global
  */
-var ui = require('./utils/ui');
+var ui = require('./utils/ui')
 
 /**
  * InvalidPageHeightError
  * @type {InvalidPageHeightError}
  * @global
  */
-var InvalidPageHeightError = Paginator.errors.InvalidPageHeightError;
+var InvalidPageHeightError = Paginator.errors.InvalidPageHeightError
+
+// Add the plugin to the tinymce PluginManager
+tinymce.PluginManager.add('paginate', tinymcePluginPaginate)
 
 /**
  * Tinymce plugin paginate
@@ -71,50 +75,125 @@ var InvalidPageHeightError = Paginator.errors.InvalidPageHeightError;
  * @param {tinymce.Editor} editor - The injected tinymce editor.
  * @returns void
  */
-function tinymcePluginPaginate(editor) {
-
+function tinymcePluginPaginate (editor) {
   /**
    * Debug all useful editor events to see the order of their happen
    * @function
    * @private
    */
-  function _debugEditorEvents(){
-    var myevents = [];
-    var mycount = {
-      init: 0,
-      change: 0,
-      nodechange: 0,
-      setcontent: 0
-    };
+  this._debugEditorEvents = _debugEditorEvents
 
-    editor.on('init',function(evt){
-      console.log(editor);
-      myevents.push({'init':evt});
-      mycount.init ++;
-      console.log(myevents,mycount);
-      // alert('pause after "init" event');
-    });
-    editor.on('change',function(evt){
-      myevents.push({'change':evt});
-      mycount.change ++;
-      console.log(myevents,mycount);
-      // alert('pause after "change" event');
-    });
-    editor.on('NodeChange',function(evt){
-      myevents.push({'NodeChange':evt});
-      mycount.nodechange ++;
-      console.log(myevents,mycount);
-      // alert('pause after "NodeChange" event');
-    });
-    editor.on('SetContent',function(evt){
-      myevents.push({'SetContent':evt});
-      mycount.setcontent ++;
-      console.log(myevents,mycount);
-      // alert('pause after "SetContent" event');
-    });
+  /**
+  * A 'Paginator' object to handle all paginating behaviors.
+  * @var {Paginator} paginator
+  * @global
+  */
+  var paginator
 
-    window.logEvents = myevents;
-    window.logCount = mycount;
+  /**
+   * A 'Display' object to handle graphics behaviors for the paginator needs.
+   * @var {Display} display
+   * @global
+   */
+  // var display
+
+  /**
+   * Is set to true when paginator is initialized.
+   * @var {Boolean} paginatorListens
+   * @global
+   */
+  var paginatorListens = false
+
+  /**
+   * Count of the iterations of watchPage() calls triggered by thrown of `InvalidPageHeightError`. This is a temporary bugfix
+   * @var {integer}
+   * @global
+   */
+  var watchPageIterationsCount = 0
+
+  /**
+   * The watch of active page is enabled if this var is true
+   * @var
+   * @global
+   */
+  var watchPageEnabled = false
+
+  // _debugEditorEvents()
+
+  this.disableWatchPage = pluginAPIDisableWatchPage
+  this.enableWatchPage = pluginAPIEnableWatchPage
+  this.getCurrentPage = PluginAPIGetCurrentPage
+
+  editor.once('init', onceEditorInit)
+  editor.on('remove', onRemoveEditor)
+  editor.on('change', onEditorChange)
+  editor.on('setContent', onEditorSetContent)
+  editor.on('NodeChange', onEditorNodeChange)
+
+  function onceEditorInit () {
+    paginator = new Paginator('A4', 'portrait', editor)
+    editor.dom.bind(editor.getDoc(), 'PageChange', onPageChange)
+    setTimeout(function () {
+      paginator.init()
+      paginator.gotoFocusedPage()
+      paginatorListens = true
+      watchPageEnabled = true
+      ui.appendNavigationButtons(paginator)
+    }, 500)
+  }
+
+  function onEditorChange (evt) {
+    var newContent
+    var oldContent
+
+    // there is both an old and a new content for this change event
+    if (evt.level && evt.lastLevel) {
+      newContent = evt.level.content
+      oldContent = evt.lastLevel.content
+
+      // the new content overidden the old one
+      if (newContent === '<p><br data-mce-bogus="1"></p>') {
+        var pagesInOldContent = $('<div>').append(oldContent).find('div[data-paginator]')
+
+        // there was at least one paginator page in the overriden old content
+        // we reset the old content then init the paginator
+        if (pagesInOldContent.length) {
+          editor.setContent(oldContent)
+          paginator.init()
+          paginator.gotoFocusedPage()
+        }
+      }
+    }
+
+    if (paginatorListens && watchPageEnabled) paginator.watchPage()
+  }
+
+  function onEditorNodeChange (evt) {
+    if (evt.element && $(evt.element).attr('data-paginator')) {
+      if (paginatorListens && watchPageEnabled) {
+        try {
+          paginator.gotoFocusedPage()
+        } catch (e) {
+          console.info("Can't go to focused page now.")
+          console.error(e.stack)
+        }
+      }
+    }
+  }
+
+  function onEditorSetContent (evt) {
+    paginatorListens = false
+    if (evt.content) {
+      paginator = new Paginator('A4', 'portrait', editor)
+      setTimeout(function () {
+        paginator.init()
+        setTimeout(function () {
+          paginator.gotoFocusedPage()
+          paginatorListens = true
+          watchPageEnabled = true
+        }, 100)
+      }, 100)
+    }
   }
 
   /**
@@ -122,16 +201,16 @@ function tinymcePluginPaginate(editor) {
    * @function
    * @private
    */
-  function onPageChange(evt){
-    ui.updatePageRankInput(evt.toPage.rank);
-    editor.nodeChanged();
+  function onPageChange (evt) {
+    ui.updatePageRankInput(evt.toPage.rank)
+    editor.nodeChanged()
   }
 
-  function onRemoveEditor(evt){
-    ui.removeNavigationButtons();
-    paginator.destroy();
-    watchPageIterationsCount = 0;
-    paginatorListens = false;
+  function onRemoveEditor (evt) {
+    ui.removeNavigationButtons()
+    paginator.destroy()
+    watchPageIterationsCount = 0
+    paginatorListens = false
   }
 
   /**
@@ -141,147 +220,93 @@ function tinymcePluginPaginate(editor) {
    * @returns void
    * @throws {Error} if error thrown is not instance of InvalidPageHeightError
    */
-  function watchPage(){
+  function watchPage () {
     try {
-      paginator.watchPage();
+      paginator.watchPage()
     } catch (e) {
-      watchPageIterationsCount++;
+      watchPageIterationsCount++
       // Due to a suspecte bug in tinymce that break the binding of DOM elements with the paginator.
       if (e instanceof InvalidPageHeightError) {
-        console.error(e.message+'... re-init paginator then watch page again...');
-        paginator.init();
-        if (watchPageIterationsCount<10) {
-          watchPage();
+        console.error(e.message + '... re-init paginator then watch page again...')
+        paginator.init()
+        if (watchPageIterationsCount < 10) {
+          watchPage()
         } else {
-          watchPageIterationsCount = 0;
+          watchPageIterationsCount = 0
         }
-      } else throw e;
+      } else throw e
     }
   }
 
   /**
-  * A 'Paginator' object to handle all paginating behaviors.
-  * @var {Paginator} paginator
-  * @global
-  */
-  var paginator;
+   * Plugin method that enable the wath of page (after used this#disableWatchPage())
+   * @method
+   * @returns void
+   */
+  function pluginAPIEnableWatchPage () {
+    watchPageEnabled = true
+  }
 
   /**
-   * A 'Display' object to handle graphics behaviors for the paginator needs.
-   * @var {Display} display
-   * @global
+   * Get the current page
+   * @returns {Page} the paginator current page.
    */
-  var display;
-
-  /**
-   * Is set to true when paginator is initialized.
-   * @var {Boolean} paginatorListens
-   * @global
-   */
-  var paginatorListens = false;
-
-  /**
-   * Count of the iterations of watchPage() calls triggered by thrown of `InvalidPageHeightError`. This is a temporary bugfix
-   * @var {integer}
-   * @global
-   */
-  var watchPageIterationsCount=0;
-
-  /**
-   * The watch of active page is enabled if this var is true
-   * @var
-   * @global
-   */
-  var watchPageEnabled = false;
-
-  // _debugEditorEvents();
+  function PluginAPIGetCurrentPage () {
+    return paginator.getCurrentPage()
+  }
 
   /**
    * Plugin method that disable the wath of page (to allow edition of extenal elements like headers and footers)
    * @method
    * @returns void
    */
-  this.disableWatchPage = function(){  // jshint ignore:line
-    watchPageEnabled = false;
-  };
-  /**
-   * Plugin method that enable the wath of page (after used this#disableWatchPage())
-   * @method
-   * @returns void
-   */
-  this.enableWatchPage = function(){ // jshint ignore:line
-    watchPageEnabled = true;
-  };
-
-  /**
-   * Get the current page
-   * @returns {Page} the paginator current page.
-   */
-  this.getCurrentPage = function(){ // jshint ignore:line
-    return paginator.getCurrentPage();
-  };
-
-  editor.once('init',function(){
-    paginator = new Paginator('A4','portrait', editor);
-    editor.dom.bind(editor.getDoc(),'PageChange',onPageChange);
-    setTimeout(function(){
-      paginator.init();
-      paginator.gotoFocusedPage();
-      paginatorListens = true;
-      watchPageEnabled = true;
-      ui.appendNavigationButtons(paginator);
-    },500);
-  });
-
-  editor.on('remove',onRemoveEditor);
-
-  editor.on('change',function(evt){
-    // var newContent, beforeContent;
-    // if (evt.level && evt.lastLevel) {
-    //     newContent = evt.level.content;
-    //     beforeContent = evt.lastLevel.content;
-    //
-    //     if (newContent === '<p><br data-mce-bogus="1"></p>') {
-    //       if ( $('div[data-paginator]', $('<div>').append(beforeContent)).length ) {
-    //         editor.setContent(beforeContent);
-    //         paginator.init();
-    //         paginator.gotoFocusedPage();
-    //       }
-    //     }
-    // }
-
-    if(paginatorListens && watchPageEnabled) paginator.watchPage();
-  });
-
-  editor.on('setContent',function(evt){
-    paginatorListens = false;
-    if (evt.content) {
-      paginator = new Paginator('A4','portrait', editor);
-      setTimeout(function(){
-        paginator.init();
-        setTimeout(function(){
-          paginator.gotoFocusedPage();
-          paginatorListens = true;
-          watchPageEnabled = true;
-        },100);
-      },100);
-    }
-  });
-
-  editor.on('NodeChange',function(evt){
-    if (evt.element && $(evt.element).attr('data-paginator')) {
-      if (paginatorListens && watchPageEnabled) {
-        try {
-          paginator.gotoFocusedPage();
-        } catch (e) {
-          console.info('Can\'t go to focused page now.');
-          console.error(e.stack);
-        }
-      }
-    }
-  });
-
+  function pluginAPIDisableWatchPage () {
+    watchPageEnabled = false
+  }
 }
 
-// Add the plugin to the tinymce PluginManager
-tinymce.PluginManager.add('paginate', tinymcePluginPaginate);
+/**
+ * A function to help to debug editor events, logging and counting each one
+ * @function
+ * @param {tinymce.Editor} The editor instance
+ * @param {Array<String>} A list of event names to handle. If undefined, debug all events.
+ * @returns void
+ */
+function _debugEditorEvents (editor, eventsToDebug) {
+  var myevents = []
+  var mycount = {
+    init: 0,
+    change: 0,
+    nodechange: 0,
+    setcontent: 0
+  }
+
+  editor.on('init', function (evt) {
+    console.log(editor)
+    myevents.push({'init': evt})
+    mycount.init++
+    console.log(myevents, mycount)
+  // alert('pause after "init" event')
+  })
+  editor.on('change', function (evt) {
+    myevents.push({'change': evt})
+    mycount.change++
+    console.log(myevents, mycount)
+  // alert('pause after "change" event')
+  })
+  editor.on('NodeChange', function (evt) {
+    myevents.push({'NodeChange': evt})
+    mycount.nodechange++
+    console.log(myevents, mycount)
+  // alert('pause after "NodeChange" event')
+  })
+  editor.on('SetContent', function (evt) {
+    myevents.push({'SetContent': evt})
+    mycount.setcontent++
+    console.log(myevents, mycount)
+  // alert('pause after "SetContent" event')
+  })
+
+  window.logEvents = myevents
+  window.logCount = mycount
+}
