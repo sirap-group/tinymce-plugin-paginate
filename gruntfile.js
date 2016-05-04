68a86751595a770e180850c92e6f0adcb4c44239
Rémi Becheras <rbecheras@sirap.fr>
2016-05-04 17:07:58 +0200

    refactor gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,78 +1,60 @@
-var shell = require("shelljs");
+var shell = require('shelljs')
 
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt)
 
-    grunt.initConfig({
-      jshint: {
-        gruntfile: ['gruntfile.js'],
-        scripts: ['scripts/**/*.js'],
-        js: {
-          options: {
-            node: true,
-            browser: true,
-            browserify: true,
-            globals: {
-              '$': true,
-              'jQuery': true,
-              'tinymce': true
-            }
-          },
-          files: {
-            src: ['src/**/*.js']
-          }
-        }
+  grunt.initConfig({
+    standard: {
+      options: { format: true },
+      gruntfile: { src: ['gruntfile.js'] },
+      scripts: { src: 'scripts/**/*.js' },
+      js: { src: 'src/**/*.js' }
+    },
+    browserify: {
+      dist: { src: 'index.js', dest: 'plugin.js' }
+    },
+    uglify: {
+      dist: {
+        files: { 'plugin.min.js': ['plugin.js'] }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: 'gruntfile.js',
+        tasks: ['standard:gruntfile']
       },
-      browserify: {
-        dist: {
-          src: 'index.js',
-          dest: 'plugin.js'
-        }
-      },
-      uglify: {
-        dist: {
-          files: {
-            'plugin.min.js': ['plugin.js']
-          }
-        }
-      },
-      watch: {
-        gruntfile: {
-          files: 'Gruntfile.js',
-          tasks: ['jshint:gruntfile'],
-        },
-        js: {
-          files: ['src/**/*.js'],
-          tasks: ['jshint:js','browserify','watch'],
-        }
-      },
-      bump: {
-        options: {
-          files: ['package.json','bower.json'],
-          updateConfigs: [],
-          commit: true,
-          commitMessage: 'Release v%VERSION%',
-          commitFiles: ['package.json','bower.json'],
-          createTag: true,
-          tagName: 'v%VERSION%',
-          tagMessage: 'Version %VERSION%',
-          push: true,
-          pushTo: 'gh-sirap-group',
-          gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-          globalReplace: false,
-          prereleaseName: false,
-          regExp: false
-        }
-      },
-    });
+      js: {
+        files: ['src/**/*.js'],
+        tasks: ['standard:js', 'browserify', 'watch']
+      }
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json', 'bower.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'gh-sirap-group',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
+    }
+  })
 
-    grunt.registerTask('jsdoc', function(){
-      shell.exec('npm run jsdoc');
-    });
+  grunt.registerTask('jsdoc', function () {
+    shell.exec('npm run jsdoc')
+  })
 
-    grunt.registerTask('build', ['jshint', 'browserify', 'uglify', 'jsdoc']);
+  grunt.registerTask('build', ['standard', 'browserify', 'uglify', 'jsdoc'])
 
-    grunt.registerTask('dev', ['jshint', 'browserify', 'watch']);
+  grunt.registerTask('dev', ['standard', 'browserify', 'watch'])
 
-    grunt.registerTask('default', ['dev']);
-};
+  grunt.registerTask('default', ['dev'])
+}
